29191c49a1f437b233faf5b9815c9e5aa7fcfa5f
Vinnie117 <hhang@hotmail.de>
2025-07-21 09:31:20 +0200

    test fixed and otsu threshold

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Load the image
 image = cv2.imread('test.jpg')
@@ -7,17 +8,38 @@ image = cv2.imread('test.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Apply binary thresholding
-_, bw_image = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-# Resize the image to make it smaller (e.g., 50% of original size)
-scale_percent = 20  # percent of original size
-width = int(bw_image.shape[1] * scale_percent / 100)
-height = int(bw_image.shape[0] * scale_percent / 100)
-dim = (width, height)
-bw_image_resized = cv2.resize(bw_image, dim, interpolation=cv2.INTER_AREA)
+#_, bw_image = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+#_, bw_otu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-# Save or display the result
-cv2.imwrite('black_and_white.jpg', bw_image)
-cv2.imshow('Black and White', bw_image_resized)
+def apply_fixed_threshold(gray_image, threshold=127):
+    # 127 is the midpoint of the 8-bit grayscale range (0 to 255),
+    _, bw = cv2.threshold(gray_image, threshold, 255, cv2.THRESH_BINARY)
+    return bw
+
+def apply_otsu_threshold(gray_image):
+    # Otsu’s method (automatic thresholding):
+    _, bw = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return bw
+
+bw_fixed = apply_fixed_threshold(gray, 127)
+bw_otsu = apply_otsu_threshold(gray)
+
+def create_image(image, scale_percent):
+    # Resize the image to make it smaller (e.g., 20% of original size)
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    return cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+
+
+img_bw_fixed = create_image(bw_fixed, 20)
+img_bw_otsu = create_image(bw_otsu, 20)
+
+
+side_by_side = np.hstack((img_bw_fixed, img_bw_otsu))
+cv2.imshow('Fixed vs Otsu Threshold', side_by_side)
+cv2.imwrite('test_bw_fixed.jpg', img_bw_fixed)
+cv2.imwrite('test_bw_otsu.jpg', img_bw_otsu)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
