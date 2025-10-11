6edc292d753d4f7d141d236f8490058f5552b41f
Vinnie117 <hhang@hotmail.de>
2025-10-11 16:54:30 +0200

    no background

--- svg.py
+++ svg.py
@@ -249,6 +249,10 @@ def build_centered_contour_axes(
     # --- Draw the contours (correct orientation) ---
     ax.contour(T, levels=levels, colors=stroke, linewidths=linewidth, origin="image")
 
+    # Make backgrounds transparent
+    fig.patch.set_alpha(0.0)     # Figure background
+    ax.patch.set_alpha(0.0)      # Axes background
+
     return fig, ax
 
 
